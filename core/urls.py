2fb66ce836daf562202aec0cc9a03fdeae2b19f6
Phạm Minh Nhật <mnhat1642005@gmail.com>
2026-03-30 07:48:09 +0700

    Add search suggestions feature with AJAX support in navbar and sidebar search forms

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('search-suggestions/', views.search_suggestions, name='search_suggestions'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
