bcb403cfb3f178c83619e1ab9f92c10873bca708
nmtoan883 <nmtoan883@gmail.com>
2026-03-30 15:44:01 +0700

    feat: initialize Django project settings with allauth, crispy-forms, and custom app configurations

--- config/settings.py
+++ config/settings.py
@@ -188,5 +188,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nmtoan883@gmail.com'
-EMAIL_HOST_PASSWORD = 'mtby cvyx qswa aegh'
+EMAIL_HOST_USER = 'Nhap email'
+EMAIL_HOST_PASSWORD = 'Nhập Mk Appassword'
