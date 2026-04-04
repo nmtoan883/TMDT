498cbb24d2ef4ce62a3d4843f424f15592880422
Tran Minh Khang <khang0802205@gmail.com>
2026-04-04 16:38:18 +0700

    feat: add blog feature

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'accounts',
     'orders',
     'coupon',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,10 @@ urlpatterns = [
     path('user/', include('accounts.urls', namespace='accounts')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('blog/', include('blog.urls')),
+    path('', views.product_list, name='product_list'),
+    path('search-suggestions/', views.search_suggestions, name='search_suggestions'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('', include('core.urls', namespace='shop')),
     path('coupon/', include('coupon.urls')),
 ]
