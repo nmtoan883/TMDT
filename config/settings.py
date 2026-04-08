d7b1d8fa39176300b91f2e20a511081504232202
nmtoan883 <nmtoan883@gmail.com>
2026-04-08 08:33:20 +0700

    build: configure collectstatic and whitenoise for production deploy

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Thêm whitenoise phục vụ build static
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,7 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Thêm thư mục Build Static
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
